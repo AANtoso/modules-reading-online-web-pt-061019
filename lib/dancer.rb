ed22dcf19e27bae81e62468af1fe348f2e89916e
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-21 23:37:11 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,9 +3,7 @@ require_relative './dance_module.rb'
 
 class Dancer
   include Dance
-  extend MetaDancing
-  include FancyDance::InstanceMethods
-
+  
   attr_accessor :name
 
   def initialize(name)
