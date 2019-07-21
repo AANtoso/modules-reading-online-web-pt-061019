220dc61550aa8ce461683e4043eb9f3983e68f45
Adjoa Ntoso <adjoaan@gmail.com>
2019-07-21 23:37:16 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,9 +3,7 @@ require_relative './class_methods_module.rb'
 
 class Kid
   include Dance
-  extend MetaDancing
-  include FancyDance::InstanceMethods
-
+  
   attr_accessor :name
 
   def initialize(name)
